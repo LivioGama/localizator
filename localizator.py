1d5d3230f5a343702eaf355857cc2f4a585bae07
Livio Gamassia <livio@mnc.ch>
2020-10-30 16:35:27 +0100

    Upgrade to Google Drive v3

--- localizator.py
+++ localizator.py
@@ -1,29 +1,35 @@
 from __future__ import print_function
 
-try:
-    import httplib2
-    import urllib3
-    from apiclient import errors
-except ImportError:
-    print("run pip3 install httplib2")
-
+import argparse
 import os
+import os.path
+import pickle
 
-try:
-    from apiclient import discovery
-except ImportError:
-    print("run `pip3 install google-api-python-client`\n "
-          "or manually on https://developers.google.com/api-client-library/python/start/installation")
-
-import oauth2client
-from oauth2client import client
-from oauth2client import tools
 import translations
+import urllib3
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 try:
-    import argparse
+    import apiclient
+except ImportError:
+    print("'import apiclient' failed; run: pip3 install google-api-python-client\n"
+          "or manually on https://developers.google.com/api-client-library/python/start/installation")
+    exit(-1)
 
-    parser = argparse.ArgumentParser(parents=[tools.argparser], description='Create localizable files')
+try:
+    import oauth2client
+    from oauth2client import client
+    from oauth2client import tools
+    from oauth2client import file
+except ImportError:
+    print("'import oauth2client' failed; run: pip3 install oauth2client")
+    exit(-1)
+
+try:
+    parser = argparse.ArgumentParser(description='Create localizable files')
     parser.add_argument('--id', help='provide file id to avoid prompt')
     parser.add_argument('--path', help='Path destination for *.lproj folders', default='./')
     parser.add_argument('--platform', choices=['ios', 'android'], help='Should be either ios or android', default='ios')
@@ -35,106 +41,80 @@ except ImportError:
     flags = None
     print("Cannot parse")
 
-SCOPES = 'https://www.googleapis.com/auth/drive'
+SCOPES = ['https://www.googleapis.com/auth/drive']
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'YOUR_PROJECT_NAME_GOES_HERE'
 
 
 def get_credentials():
-    """Gets valid user credentials from storage.
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir, APPLICATION_NAME + '.json')
-
-    store = oauth2client.file.Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else:  # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CLIENT_SECRET_FILE, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    return creds
 
 
-def getFiles(service):
-    """
-    Retrieve a list of File resources.
-    Args:
-    service: Drive API service instance.
-    Returns:
-    List of File resources.
-    """
-    result = []
-    page_token = None
-    while True:
-        try:
-            param = {}
-            if page_token:
-                param['pageToken'] = page_token
-                param['maxResults'] = '1000'
-            files = service.files().list(**param).execute()
+def get_files(service):
+    results = service.files().list(fields='files(id, exportLinks)').execute()
+    items = results.get('files', [])
+    if not items:
+        print('No files found.')
+    else:
+        return service, items
 
-            result.extend(files['items'])
-            page_token = files.get('nextPageToken')
-            if not page_token:
-                break
-        except errors.HttpError as error:
-            print('An error occurred: %s' % error)
-            break
-    return service, result
-
-
-def download__file_metadata(file_id, token, gid=0):
-    file_id = file_id
-    url = "https://docs.google.com/spreadsheets/d/"+file_id+"/export?gid="+str(gid)+"&format=csv"
-
-    headers = {"Authorization": "Bearer "+str(token)}
-    r = urllib3.PoolManager().request('GET', url=url, headers=headers)
-    return r.data
 
 def main():
-    """Shows basic usage of the Google Drive API.
-    Creates a Google Drive API service object and outputs the names and IDs
-    for up to 10 files.
-    """
     if not os.path.exists(CLIENT_SECRET_FILE):
-        print('Enable the API and generate your ' + CLIENT_SECRET_FILE + ' and drag it in the same directory than this script '
-                                                                         'https://console.developers.google.com/flows/enableapi?apiid=drive')
-
+        print("'" + CLIENT_SECRET_FILE + "' file does not exist.\n"
+                                         "Enable the API and save the '" + CLIENT_SECRET_FILE + "' file here.\n"
+                                                                                                "https://console.developers.google.com/flows/enableapi?apiid=drive")
         exit(-1)
+
     credentials = get_credentials()
-
-
-    credentials.authorize(httplib2.Http())
-    token = str(credentials.access_token)
-
+    print("Authorizing...")
+    service = build('drive', 'v3', credentials=credentials)
+    print("Listing files...")
+    service, files = get_files(service)
 
     if args.id:
-        file = download__file_metadata(args.id, token, args.gid)
+        for item in files:
+            if item['id'] == args.id:
+                file = item
+        if not 'file' in locals():
+            print("File with id '" + args.id + "' not found in files list.\n"
+                                               "Notice: you need to load the file at least once in your browser, so it's visible in your files list.")
+            exit(-1)
     else:
         i = 0
-        service, files = getFiles(service)
         for item in files:
-            print(str(item['title']) + " - " + str(item['id']))
+            print("[" + str(i) + "] " + str(item['title']) + " - " + str(item['id']))
             i += 1
-        exit(1)
+        isDigit = False
+        while not isDigit:
+            index = input("Select a file index: \n")
+            isDigit = index.isdigit()
+            if int(index) > len(files) or int(index) < 0:
+                print("Invalid index supplied. Try again")
+                isDigit = False
+        file = files[int(index)]
 
-    content = file
-    filename = "tmp" + '.csv'
+    content = download_file(service, file, args.gid)
+    filename = 'tmp.csv'
     csvf = open(filename, 'w')
     csvf.write(content.decode("utf-8"))
     csvf.close()
+
     if args.platform == 'ios':
         translations.translate(filename, args.path)
     elif args.platform == 'android':
@@ -143,7 +123,20 @@ def main():
         print("Invalid platform. type --help for help")
     if not args.keep_csv:
         os.remove(filename)
-    print("Your files have been generated under '"+args.path+"'")
+    print("Success! Localization files for platform " + args.platform + " have been generated under '" + args.path + "'")
+
+
+def download_file(service, drive_file, tab_id):
+    download_url = drive_file['exportLinks']['text/csv'] + "&gid=" + tab_id
+    print("Downloading " + download_url)
+    resp, content = service._http.request(download_url)
+    if resp.status == 200:
+        return content
+    else:
+        print('HTTP error: %s' % resp.status)
+        if args.gid:
+            print('Maybe --gid is wrong?')
+        exit(-1)
 
 
 if __name__ == '__main__':
